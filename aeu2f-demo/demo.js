b64d49a1bcaaeeb95ce16b106f4fa4f31630d797
Brian M Hunt <brianmhunt@gmail.com>
2016-02-03 14:21:47 -0500

    Add some useful comments to demo.js

--- aeu2f-demo/demo.js
+++ aeu2f-demo/demo.js
@@ -3,7 +3,9 @@
 // License: MIT
 //
 
-// Action is used to keep track of steps of the process.
+// Action is used to keep track of steps of the process.  It is only used
+// for informational purposes and has no impact on the actual signing and
+// registration.
 function Action(name, data, style) {
   this.name = name
   this.data = data ? JSON.stringify(data, null, 2) : ''
@@ -26,25 +28,39 @@ Action.add = function(name, data, style) {
   actions.unshift(new Action(name, data, style))
 }
 
+//
+// ---  The signing steps are below ---
+//
+
 
 function on_fail(msg) {
   Action.add("Transmission failed", msg, 'fail')
   throw new Error("Transmission failed.")
 }
 
+/*
+  request - Communicate with the server.
 
+  Either GET a Challenge (to be completed by the U2F device), or POST
+  the response completed by the U2F device.
+ */
 function request(type, url, data) {
   if (is_communicating()) { return }
   is_communicating(true)
   Action.add("[" + type + "] " + url, data, 'info')
 
-  // GET the Challenges or POST the Responses
   return $[type](url, data ? JSON.stringify(data) : undefined)
     .always(function () { is_communicating(false) })
     .fail(on_fail)
 }
 
 
+
+/*
+  getU2FResponseToChallenge  -  Wait for the U2F interface to respond.
+
+  Return a promise, so we keep to an easy-to-illustrate set of steps, below.
+ */
 function getU2FResponseToChallenge(kind, req) {
   Action.add("U2F Challenged to: " + kind, req, 'warn')
   waiting_for_key(true)
@@ -85,6 +101,12 @@ var Model = {
 
 
 function sendChallengeResponse(url, resp) {
+  // The resp could contain an errorCode, as per:
+  // https://developers.yubico.com/U2F/Libraries/Client_error_codes.html
+  if (resp.errorCode) {
+    Action.add("U2F Failed.", resp, 'fail')
+    throw new Error("U2F failed, error code: ", resp.errorCode)
+  }
   return request("post", url, resp).fail(on_fail)
 }
 
