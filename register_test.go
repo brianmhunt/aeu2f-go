6ba06969d3fc13f625053553323e58d7c62828de
Brian Hunt <brianmhunt@gmail.com>
2016-02-13 10:32:22 -0500

    lint + remove more dead test code

--- register_test.go
+++ register_test.go
@@ -42,32 +42,10 @@ var fakeRegistrationChallenge = u2f.Challenge{
   TrustedFacets: []string{fakeHost},
   Challenge: fakeChallenge }
 
-
-var fakeRegistrationResponse2 = u2f.RegisterResponse{
-  ClientData: "eyJvcmlnaW4iOiAiaHR0cDovL2xvY2FsaG9zdDo4MDgxIiwgImNoYWxsZW5nZSI6ICJEMnB6VFBaYTdicTY5QUJ1aUdRSUxvOXpjc1RVUlAyNlJMaWZUeUNraWxjIiwgInR5cCI6ICJuYXZpZ2F0b3IuaWQuZmluaXNoRW5yb2xsbWVudCJ9",
-  RegistrationData: "BQSivQtJ6-lAgZ2qQ0aUGLEiJSRoLWUSGcmMO8C-GuibA0-xTvmuQfTqKyFJZWOUjGzEIgF4xV6gJ6itcagsyuUWQEQh9noDSu-WtzTOMhK_lKHxwHtQgJHCkzs4mukfpf310K5Dq9k6zBNtZ2RMBWgJhI7hJo4JiFn3k2GUNLwKZpwwggGHMIIBLqADAgECAgkAmb7osQyi7BwwCQYHKoZIzj0EATAhMR8wHQYDVQQDDBZZdWJpY28gVTJGIFNvZnQgRGV2aWNlMB4XDTEzMDcxNzE0MjEwM1oXDTE2MDcxNjE0MjEwM1owITEfMB0GA1UEAwwWWXViaWNvIFUyRiBTb2Z0IERldmljZTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABDvhl91zfpg9n7DeCedcQ8gGXUnemiXoi-JEAxz-EIhkVsMPAyzhtJZ4V3CqMZ-MOUgICt2aMxacMX9cIa8dgS2jUDBOMB0GA1UdDgQWBBQNqL-TV04iaO6mS5tjGE6ShfexnjAfBgNVHSMEGDAWgBQNqL-TV04iaO6mS5tjGE6ShfexnjAMBgNVHRMEBTADAQH_MAkGByqGSM49BAEDSAAwRQIgXJWZdbvOWdhVaG7IJtn44o21Kmi8EHsDk4cAfnZ0r38CIQD6ZPi3Pl4lXxbY7BXFyrpkiOvCpdyNdLLYbSTbvIBQOTBFAiEA1uwJKNez6_BHdA2d-DPmRFJj19biYNkhN86SFH5Z_lYCICld2L3ZAVsm_uNFRt13_N9dlhGu50pb1ql8-_3_p5v1" }
-
-
 // From client
 var fakeRegistrationResponse = u2f.RegisterResponse{
   RegistrationData: "BQRM6zTJH7HRlC3yR4nO25ibCNXNRCsiyVjK6T1xa67lvbSDdzjvcvNoSW8pllLf6DWWX5j-7oTOYXdSiATuJ8K0QJNwNOkQqIBLOLtFxEBs6rtKiUc1D6rrGyexWCKsxElDFgPkvYyG88Vzjfej2dqYFEjHVTvLc4GRnZObENSe3tkwggJEMIIBLqADAgECAgR4wN8OMAsGCSqGSIb3DQEBCzAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDYzMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowKjEoMCYGA1UEAwwfWXViaWNvIFUyRiBFRSBTZXJpYWwgMjAyNTkwNTkzNDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABLW4cVyD_f4OoVxFd6yFjfSMF2_eh53K9Lg9QNMg8m-t5iX89_XIr9g1GPjbniHsCDsYRYDHF-xKRwuWim-6P2-jOzA5MCIGCSsGAQQBgsQKAgQVMS4zLjYuMS40LjEuNDE0ODIuMS4xMBMGCysGAQQBguUcAgEBBAQDAgUgMAsGCSqGSIb3DQEBCwOCAQEAPvar9kqRawv5lJON3JU04FRAAmhWeKcsQ6er5l2QZf9h9FHOijru2GaJ0ZC5UK8AelTRMe7wb-JrTqe7PjK3kgWl36dgBDRT40r4RMN81KhfjFwthw4KKLK37UQCQf2zeSsgdrDhivqbQy7u_CZYugkFxBskqTxuyLum1W8z6NZT189r1QFUVaJll0D33MUcwDFgnNA-ps3pOZ7KCHYykHY_tMjQD1aQaaElSQBq67BqIaIU5JmYN7Qp6B1-VtM6VJLdOhYcgpOVQIGqfu90nDpWPb3X26OVzEc-RGltQZGFwkN6yDrAZMHL5HIn_3obd8fV6gw2fUX2ML2ZjVmybjBGAiEA_V8dGq-W1WwEO4LM8VEsNWAeK6GjxCC1ihqHW_K9H74CIQCcouyEm3V9DlqmOeJbe7qyki6f30qkiUfEBXVAAmolJg",
   ClientData: "eyJ0eXAiOiJuYXZpZ2F0b3IuaWQuZmluaXNoRW5yb2xsbWVudCIsImNoYWxsZW5nZSI6IlJIbGowZ0twalctbGJ4ZVA2a1NFU05HbGcydXJJZGJmWW5xS0FoN0h4bG8iLCJvcmlnaW4iOiJodHRwczovL2FldTJmLWRlbW8uYXBwc3BvdC5jb20iLCJjaWRfcHVia2V5Ijp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4Ijoib0RxWGxjNEhYY2tvTDFxWnMxbTlIWEdvVllKTHB1d3FCUzJFWnJZaXBqOCIsInkiOiI1b2xZNlJYalBXOWhrUXoyX0dLckd4dGFHYjRmN1Y0aUZVYVdxQm1EaVFzIn19"}
-
-
-
-//
-// func TestDatabaseIdempotency(t *testing.T) {
-//   // Ensure what we put in is what we get out.
-//   key := datastore.NewKey(ctx, "Challenge", 0, 0, nil)
-//   _, err = datastore.Put(ctx, key, &fakeRegistrationChallenge)
-// 	if err != nil {
-// 		t.Fatalf("datastore.Put error: %v", err)
-// 	}
-//   regCopy = new(u2f.Challenge)
-//
-//   // Note that the datastore rounds Timestamp to microsecond precision
-//
-// }
 
 
 func TestNewChallenge(t *testing.T) {
@@ -120,22 +98,22 @@ func TestGoodRegistration(t *testing.T) {
   }
   defer ctx.Close()
 
-  var testId = "test-id-🔒"
+  var testID = "test-id-🔒"
 
   // Mimic NewChallenge
-  ckey := makeKey(ctx, testId, "Challenge")
+  ckey := makeKey(ctx, testID, "Challenge")
   _, err = datastore.Put(ctx, ckey, &fakeRegistrationChallenge)
 	if err != nil {
 		t.Fatalf("datastore.Put error: %v", err)
 	}
   // log.Printf("Challenge: %+v", fakeRegistrationChallenge)
 
-  if err := StoreResponse(ctx, testId, fakeRegistrationResponse); err != nil {
+  if err := StoreResponse(ctx, testID, fakeRegistrationResponse); err != nil {
     t.Fatalf("StoreRegistration: %v", err)
   }
 
   // Load what was just saved and verify it.
-  k := makeKey(ctx, testId, "Registration")
+  k := makeKey(ctx, testID, "Registration")
   var regi Registration
   if err := datastore.Get(ctx, k, &regi); err != nil {
     t.Fatalf("datastore.Get (%+v): %+v", k, regi)
@@ -146,9 +124,9 @@ func TestGoodRegistration(t *testing.T) {
     t.Error("Expected Counter to be 0")
   }
 
-  if regi.UserIdentity != testId {
+  if regi.UserIdentity != testID {
     t.Error("Expected user identity %v to be %v", regi.UserIdentity,
-      testId)
+      testID)
   }
 
   u2fReg := new(u2f.Registration)
