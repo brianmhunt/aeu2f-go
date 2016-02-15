8f7fb8e4f1df199597421c3df80123e31216bf55
Brian Hunt <brianmhunt@gmail.com>
2016-02-15 10:30:28 -0500

    Add `serve.sh` for starting the test server

--- serve.sh
+++ serve.sh
@@ -1,11 +1,32 @@
-#!/bin/sh
-
-# We'll want to kill goapp when this exits.
-# See e.g. http://stackoverflow.com/questions/360201
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+#!/usr/bin/env bash -m
 
 # Start the AppEngine Server, in the background.
-goapp serve ./aeu2f-demo &
+# We don't want to use goapp here because it creates more child
+# processes that we'd need to clean up.
+dev_appserver.py ./aeu2f-demo &
+GOAPP_PID=$!
 
 # Connect with ngrok.
-ngrok -proto https 8080
+ngrok -log stdout -proto https 8080 > grok.log &
+NGROK_PID=$!
+
+# Wait for ngrok to connect
+sleep 2
+
+# Tell the user what the tunnel is
+echo
+echo "                             ⭐️ ⭐️ ⭐️   HTTPS Tunnel details  ⭐️ ⭐️ ⭐️ "
+cat grok.log | grep "Tunnel established"
+
+# Echo the jobs
+echo "GOAPP PID ${GOAPP_PID} / NGROK PID ${NGROK_PID}"
+jobs
+
+# We'll want to kill the kids when this exits.
+# See e.g. http://stackoverflow.com/questions/360201
+trap "trap - SIGTERM && kill -- $GOAPP_PID $NGROK_PID" SIGINT SIGTERM EXIT
+
+# Wait for processes to finish
+# See e.g. http://stackoverflow.com/questions/356100
+wait $GOAPP_PID
+wait $NGROK_PID
